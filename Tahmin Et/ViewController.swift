45beb2c22c34739ccb2e77d1567304da0a8ed487
Semih Toy <ssemih4238@gmail.com>
2019-07-04 12:43:02 +0300

    Initial Commit

---
+++ Tahmin Et/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tahmin Et
+//
+//  Created by Xcode on 4.07.2019.
+//  Copyright © 2019 Xcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
